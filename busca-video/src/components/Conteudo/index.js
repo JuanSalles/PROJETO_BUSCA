fa10df893f5aaeec2bfbad651a9fcbbbd327b017
Juan Salles <juanprazeres@gmail.com>
2023-04-14 13:25:40 -0300

    implementando a logica

--- busca-video/src/components/Conteudo/index.js
+++ busca-video/src/components/Conteudo/index.js
@@ -1,18 +1,80 @@
 import './Conteudo.css'
 
-const Conteudo = () => {
 
+const Conteudo = () => {
+  
     return(
+       
         <div className='styleConteudo'>
 
-          <img src="./Imagens/ferrari.png" alt="Ferrari vermelha" className="Carro"></img>
-          <img src="./Imagens/ps5.png" alt="Ferrari vermelha" className="Games"></img>
-          <img src="./Imagens/comida-arabe.png" alt="Ferrari vermelha" className="comida"></img>
-          <img src="./Imagens/skate.png" alt="Ferrari vermelha" className="skate"></img>
-          <img src="./Imagens/moqueca.png" alt="Ferrari vermelha" className="comida"></img>
-          <img src="./Imagens/porsche.png" alt="Ferrari vermelha" className="Carro"></img>
+          <Exibir/>
           
         </div>
     )
 }
 export default Conteudo;
+
+var videos = {
+  video1: {
+      address: "./Imagens/ferrari.png",
+      desc: "Carro de luxo da Ferrari na cor vermelha",
+      classe: "Carro"
+  },
+  video2: {
+      address: "./Imagens/ps5.png",
+      desc: "video game playstation 5",
+      classe: "game"
+  },
+  video3: {
+      address: "./Imagens/comida-arabe.png",
+      desc: "comida da regiao do oriente medio",
+      classe: "comida"
+  },
+  video4: {
+      address: "./Imagens/skate.png",
+      desc: "skatista descendo uma rampa",
+      classe: "esporte"
+  },
+  video5: {
+      address: "./Imagens/moqueca.png",
+      desc: "comida tipica do nordeste feita normalmente com frutos do mar",
+      classe: "comida"
+  },
+  video6: {
+      address: "./Imagens/porsche.png",
+      desc: "Carro de luxo da marca porsche",
+      classe: "carro"
+  }
+}
+
+
+const busca = (nome) => {
+  var resultado = {};
+  let teste = 0;
+
+  for (let i=0; i<Object.keys(videos).length ; i++){
+    
+    
+    switch (videos[`video${i+1}`].classe){
+      case nome:
+        resultado[teste] = videos[`video${i+1}`];
+        teste++;
+        break;
+      default:
+    }
+
+  } 
+  return(resultado);
+} ;
+
+const Exibir = () => {
+  var resultado = busca("carro");
+
+  for(let a=0; a<Object.keys(resultado).length; a++){
+    return(
+      <img src={resultado[a].address} alt={resultado[a].desc} className={resultado[a].classe}/>
+    )
+  }
+  
+
+}
